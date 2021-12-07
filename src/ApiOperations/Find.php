6e94a650f9381b716030945a1704743fcb6d3f84
Arunas Skirius <arukomp@gmail.com>
2021-12-07 12:46:13 +0200

    add one more missing change

--- src/ApiOperations/Find.php
+++ src/ApiOperations/Find.php
@@ -3,6 +3,11 @@
 namespace Belltastic\ApiOperations;
 
 use Belltastic\ApiClient;
+use Belltastic\Exceptions\ForbiddenException;
+use Belltastic\Exceptions\NotFoundException;
+use Belltastic\Exceptions\UnauthorizedException;
+use Belltastic\Exceptions\ValidationException;
+use GuzzleHttp\Exception\GuzzleException;
 
 trait Find
 {
@@ -12,8 +17,13 @@ trait Find
      * Re-fetch the model from the API
      *
      * @return $this
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     * @throws UnauthorizedException
+     * @throws ValidationException
+     * @throws GuzzleException
      */
-    public function refresh()
+    public function refresh(): self
     {
         $client = new ApiClient($this->_apiKey, $this->_options ?? []);
 
@@ -24,7 +34,14 @@ trait Find
         return $this;
     }
 
-    public static function find($id, $options = [])
+    /**
+     * @throws NotFoundException
+     * @throws ForbiddenException
+     * @throws UnauthorizedException
+     * @throws GuzzleException
+     * @throws ValidationException
+     */
+    public static function find($id, $options = []): self
     {
         $instance = new static(['id' => $id], $options);
         $instance->refresh();
